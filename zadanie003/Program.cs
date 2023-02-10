53529998646b92e48724957e075688e89dfd8f01
Daniil <daniil.ivolga27011@mail.ru>
2023-02-10 15:49:07 +0300

    ЗАДАНИЕ 3

---
+++ zadanie003/Program.cs
@@ -0,0 +1,35 @@
+﻿/*int N;
+Console.Writline("Введите N");
+N = 5;
+int index;
+index = -N;
+while (index <= N) ;
+{
+    Console.Writline(index + " ");
+    index = index + 1;
+}
+console.WriteLine("end");
+*/
+/*Console.WriteLine("Введите x, y");
+ int x =Convert.ToInt32(Console.ReadLine());
+ int y =Convert.ToInt32(Console.ReadLine());
+ if (x==y*y)
+ {
+Console.WriteLine("Является");
+
+ }
+ else
+ {
+Console.WriteLine("Не является");
+ }
+*/
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = 0;
+y  = x*x;
+Console.WriteLine(y);
+
+
+
+ 
