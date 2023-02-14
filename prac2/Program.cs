77491a8ed388f3d926ffd1e5e4eec6b92bbfa7b2
Daniil <daniil.ivolga27011@mail.ru>
2023-02-14 14:49:28 +0300

    семинарные задачи

---
+++ prac2/Program.cs
@@ -0,0 +1,73 @@
+﻿/*using System;
+
+public class MyProgramvo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        
+        int x = Convert.ToInt32(Console.ReadLine());
+        if (x % 7 == 0)
+        {
+            if (x % 23 == 0)
+            {
+                Console.WriteLine("Кратно");
+            }
+            else
+            {
+                Console.WriteLine("Не кратно");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Не кратно");
+        }
+    }
+}
+*/
+ 
+ /*5 Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+5, 25  ->  да
+-4, 16  ->  да
+25, 5  ->  да
+8,9  ->  нет
+*/
+/*
+Console.WriteLine("Введите 2 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a==b*b)
+{
+Console.WriteLine("числа подходит");
+}
+else if (b==a*a)
+{
+Console.WriteLine("числа подходит");    
+}
+else
+{
+Console.WriteLine("Числа не являются квадратами друг друга");
+}
+*/
+
+/*3 Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.?/
+
+34, 5 -> не кратно, остаток 4 
+16, 4 -> кратно
+*/
+
+/*Console.WriteLine("Введите 2 числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a%b==0)
+{
+Console.WriteLine("Число кратно");
+}
+else if (a%b>0)
+{
+ // Console.WriteLine("Делится остатком");
+  Console.WriteLine(a%b);
+}
+*/
+
