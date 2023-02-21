5c77007783d00bff2b725d9802f412a4d80c7d61
Daniil <daniil.ivolga27011@mail.ru>
2023-02-21 16:53:30 +0300

    code

---
+++ prac4/Program.cs
@@ -0,0 +1,14 @@
+﻿string GetHtml(string url, string path)
+{
+  string html = String.Empty;
+  if (!File.Exists(path))
+  {
+    html = new HttpClient().GetStringAsync(url).Result;
+    File.WriteAllText(path, html);
+  }
+  else
+  {
+    html = File.ReadAllText(path);
+  }
+  return html;
+}
