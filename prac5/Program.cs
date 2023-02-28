1334a6ec3c7245b11bc6fbc361119bdd7d779737
Daniil <daniil.ivolga27011@mail.ru>
2023-02-28 16:07:12 +0300

    matrix

--- prac5/Program.cs
+++ prac5/Program.cs
@@ -1,14 +1,4 @@
 ﻿
-string[,] table = new string[3, 5];
-table[1, 2] = "слово";
-for (int rows = 0; rows < 2; rows++)
-{
-    for (int columns = 0; columns < 5; columns++)
-    {
-        Console.WriteLine($"-{table[rows, columns]}-");
-    }
-}
-
 
 
 
@@ -26,7 +16,6 @@ for (int i = 0; i < matrix.GetLength(0); i++)
     Console.WriteLine();
 }
 }
-
 void FillArray(int[,]matrix)
 {
     for(int i = 0; i < matrix.GetLength(0); i++)
@@ -37,34 +26,34 @@ void FillArray(int[,]matrix)
         }
     }
 }
-int[,] matrix = new int[3, 4];
+int[,] matrix = new int[10, 10];
+void FillArray2(int[,]matrix2)
+{
+    for(int i = 0; i < matrix2.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix2.GetLength(1); j++)
+        {
+           matrix2[i,j] =  new Random().Next(1, 10);
+        }
+    }
+}
+int[,] matrix2 = new int[10, 10];
+void PrintArray2(int[,] matrix2)
+{
 
-PrintArray(matrix);
-Console.WriteLine();
+for (int i = 0; i < matrix2.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix2.GetLength(1); j++)
+    {
+        Console.Write($"{matrix2[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+
 FillArray(matrix);
 PrintArray(matrix);
-
-
-
-
-
-int Factorial (int n)
-{
-    if (n == 1) return 1;
-    else  return n * Factorial(n - 1);
-}
-Console.WriteLine(Factorial(5));
-
-
-
-
-int Fibonacci (int n)
-{
-    if (n == 1 || n==2) return 1;
-    else  return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
-
-for (int i = 1; i < 50; i++)
-{
-Console.WriteLine($"f{i} = {Fibonacci(i)}");
-}
+Console.WriteLine();
+FillArray2(matrix2);
+PrintArray2(matrix2);
